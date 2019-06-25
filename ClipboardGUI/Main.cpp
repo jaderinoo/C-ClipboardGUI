0427b43c5d81f2d70de57d8ec622842fdb07169c
Jad El-Khatib <jaderinoo@gmail.com>
2019-06-24 21:03:07 -0400

    Initial Commit

---
+++ ClipboardGUI/Main.cpp
@@ -0,0 +1,67 @@
+#include "pch.h"
+#include "stdafx.h"
+
+using namespace std;
+
+void clippy();
+
+int main()
+{
+	clippy();
+}
+
+void clippy()
+{
+	//Initialization
+	string copy("Initialization");
+	string compare("compare");
+	char currentTime[32]{};
+
+	//Pulls initial "copy" from clipboard and sets compare = copy
+	if (OpenClipboard(NULL))
+	{
+		HANDLE clip;
+		clip = GetClipboardData(CF_TEXT);
+		//Lock and copy
+		copy = (LPSTR)GlobalLock(clip);
+		compare = copy;
+		//Unlock 
+		GlobalUnlock(clip);
+		CloseClipboard();
+	}
+
+	//Grabs current time
+	time_t a = time(nullptr);
+	struct tm time_info;
+
+	//Formats the time
+	if (localtime_s(&time_info, &a) == 0) strftime(currentTime, sizeof(currentTime), "%H:%M:%S", &time_info);
+
+	//Prints current time
+	cout << currentTime;
+
+	//Prints updated clipboard
+	cout << " - Clipboard updated: " + copy + " \n" << endl;
+
+	//Loop and check if copy and compare are the same
+	while (copy == compare) {
+
+		//Wait for 5 seconds before each check 
+		//This can be changed to any amount of time
+		Sleep(5000);
+
+		if (OpenClipboard(NULL))
+		{
+			HANDLE clip;
+			clip = GetClipboardData(CF_TEXT);
+			//Lock and copy
+			compare = (LPSTR)GlobalLock(clip);
+			//Unlock 
+			GlobalUnlock(clip);
+			CloseClipboard();
+		}
+	}
+
+	//If not, return to main and call recursively 
+	main();
+}
---
+++ ClipboardGUI/stdafx.h
@@ -0,0 +1,30 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+#include <winuser.h>
+#include <winbase.h>
+#include <chrono>
+#include <ctime> 
+#include <string> 
+#include "stdafx.h"
+
+
+// reference additional headers your program requires here
